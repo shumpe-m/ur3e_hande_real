d1dad4c75f72b9312658476c5731a9a43b3a25df
shumpe-m <d1301539@gmail.com>
2023-04-10 14:46:23 +0900

    Fix

--- ur_control_scripts/setup.py
+++ ur_control_scripts/setup.py
@@ -4,8 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['ur_control_moveit'],
-    packages=['motion_capture'],
+    packages=['ur_control_moveit', 'motion_capture'],
     package_dir={'': 'src'}
 )
 

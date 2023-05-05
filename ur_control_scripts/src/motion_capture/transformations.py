8fac435797b884404bfc75851770a2a3905e8fb6
shumpe-m <d1301539@gmail.com>
2023-05-05 16:40:19 +0900

    check to pick and speed up

--- ur_control_scripts/src/motion_capture/transformations.py
+++ ur_control_scripts/src/motion_capture/transformations.py
@@ -4,7 +4,44 @@ import copy
 
 import geometry_msgs.msg
 
-class Transformations:
+class Transformations(object):
+    def __init__(self):
+        super(Transformations, self).__init__()
+
+    # def rot(self, rpy):
+    #     """
+    #     This function calculates a rotation matrix.
+    #     It includes right-handed to left-handed conversions.
+
+    #     Parameters
+    #     ----------
+    #     rpy : list [roll, pitch, yaw]
+    #         rpy is roll, pitch, and yaw.
+
+    #     Returns
+    #     -------
+    #     R : numpy.ndarray
+    #         Rotation matrix in 3 dimensions.
+    #     """
+    #     roll = rpy[0]
+    #     pitch = rpy[1]
+    #     yaw = rpy[2]
+
+    #     Rx = np.array([[1, 0, 0],
+    #                 [0, np.cos(roll), np.sin(roll)],
+    #                 [0, -np.sin(roll), np.cos(roll)]])
+
+    #     Ry = np.array([[np.cos(pitch), 0, -np.sin(pitch)],
+    #                 [0, 1, 0],
+    #                 [np.sin(pitch), 0, np.cos(pitch)]])
+
+    #     Rz = np.array([[np.cos(yaw), np.sin(yaw), 0],
+    #                 [-np.sin(yaw), np.cos(yaw), 0],
+    #                 [0, 0, 1]])
+
+    #     R = Rz.T.dot(Ry.T).dot(Rx.T) # .T = transform right handed axis to left handed axis
+
+    #     return R
 
     def transform_leftHanded_to_rightHanded(self, local_pose, offset):
         """
